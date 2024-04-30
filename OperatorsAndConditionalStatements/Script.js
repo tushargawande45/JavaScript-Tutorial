e55d55404d04fcd7021b39997955a536897c5621
Tushar Gawande <tgawande04@gmail.com>
2024-04-30 18:02:29 +0530

    Operators And Conditional Statements

---
+++ OperatorsAndConditionalStatements/Script.js
@@ -0,0 +1,120 @@
+/*
+//Arithmetic Operators
+let a = 5;
+let b = 2;
+console.log("a =",a," & b =",b);
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);
+*/
+
+//Unary Operators
+/*
+let a = 5;
+let b = 2;
+console.log("a =", a , "&" ,"b =" , b)
+a--;
+console.log(a) //4 
+*/
+/*
+let a = 5;
+let b = 2;
+++a;
+console.log("++a =", ++a) //7
+console.log(a) //7
+*/
+/*
+let a = 5;
+let b = 2;
+a++
+console.log("a++ =", a++) //6
+console.log(a) //7
+*/
+
+/*
+//Assignment Operators
+let a = 5;
+let b = 2;
+
+// a +=1; 
+// console.log(a) //6
+// a -=1; 
+// console.log(a) //4
+// a *=1; 
+// console.log(a) //5
+// a /=1; 
+// console.log(a) //5
+// a %=1; 
+// console.log(a) //0
+// a **=2; 
+// console.log(a) //25
+*/
+
+//Comparison Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b) //false
+// console.log("5 != 2", a != b) //true
+
+// let a = 5;
+// let b = '5';
+
+// console.log("a==b" , a==b)  // true // checks only value not type
+// console.log("a===b" , a===b) // false // checks both value and its type
+// console.log("a!==b" , a!==b) // true
+
+//Logical Operators
+//&& -> logical and 
+//|| -> logical or
+//!  -> logical not
+
+//Conditional Statements
+//1) if statements 
+// if(){
+
+// }
+
+
+// 2)if else statements
+//  if(){
+
+// }else{
+
+// }
+
+// 3)else if statements
+// if(){
+
+// }else if(){
+
+// }else{
+
+// }
+
+
+//Ternary Operators
+// let age = 16
+
+// let result = age >= 18 ? "Adult" : "Not Adult";
+// console.log(result);
+
+
+//Switch Statements
+// const expr = 'Papayas';
+// switch (expr) {
+//   case 'Oranges':
+//     console.log('Oranges are $0.59 a pound.');
+//     break;
+//   case 'Mangoes':
+//   case 'Papayas':
+//     console.log('Mangoes and papayas are $2.79 a pound.');
+//     // Expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${expr}.`);
+// }
+
