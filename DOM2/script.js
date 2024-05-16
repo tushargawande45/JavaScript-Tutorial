5cc178ec65bf56c50d762af7b93a8d780c032eaa
Tushar Gawande <tgawande04@gmail.com>
2024-05-16 12:19:49 +0530

    Inserting and Deleting elements

---
+++ DOM2/script.js
@@ -0,0 +1,28 @@
+// DOM Manipulations:
+// *Atrributes:
+// 1)getAttribute(attr) //to get the attribute value
+// 2)setAttribute(attr,value) //to set the attribute value 
+
+// *Style:
+// 1)Node.style
+
+
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class","newClass"));
+
+// let div = document.querySelector("div");
+// div.style.backgroundColor = "green";
+// div.style.backgroundColor = "purple";
+// div.style.fontSize = "25px";
---
+++ DOM2/script2.js
@@ -0,0 +1,35 @@
+//Dom Manipulation
+// *Insert Element:
+// 1)node.append(el) //adds at the end of the node(inside)
+// 2)node.prepend(el) //adds at the start of the node)inside
+// 3)node.before(el) //adds before the node (outside)
+// 4)node.after(el) //adds after the node(outside)
+
+// *Delete Element:
+// 1)node.remove() //remove the node
+
+
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click me!";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// div.append(newBtn);
+
+// let div = document.querySelector("div");
+// div.prepend(newBtn);
+
+// let div = document.querySelector("div");
+// div.before(newBtn);
+
+// let p = document.querySelector("p");
+// p.after(newBtn);
+
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i> Hi, I am new!</i>";
+// let b = document.querySelector("body");
+// b.prepend(newHeading);
+
+// newHeading.remove();
